1b740e2e384a900ead0794b77de99d4dcd0881e8
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-02-05 17:05:45 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function insert(number){
+function insert(number) {
   let num = document.getElementById('input').innerHTML;
-  let br = document.getElementById('input').innerHTML = number + num;
-   
-  if(br.length == 11) {
-    document.getElementById('input').innerHTML = '<br>'
-  }
+  document.getElementById('input').innerHTML = number + num;
 
 }
-   
 
  function clean() {
   document.getElementById('input').innerHTML= "";
  }
 
+function res() {
+  let input = document.getElementById('input').innerHTML;
+
+  if(input){
+    document.getElementById('input').innerHTML = eval(input);
+  }
+}
